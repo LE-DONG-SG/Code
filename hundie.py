8e72a7b53d6c80762ebc1bbedf8a20e3fc0b7020
dongqingqi-blip <dongqingqi@163.com>
2026-03-29 17:06:36 +0800

    Add files via upload

--- hundie.py
+++ hundie.py
@@ -90,9 +90,9 @@ with tab_alias:
         with col2:
             f_noise = st.slider("高频干扰频率 (Hz)", 35, 100, 60, key="f_noise")
         with col3:
-            fs = st.slider("采样频率 fs (Hz)", 1, 75, 100, key="fs_filter")
+            fs = st.slider("采样频率 fs (Hz)", 75, 100, 80, key="fs_filter")
 
-        fc = st.slider("滤波器截止频率 (Hz)", 1, 50, 30, key="fc")
+        fc = st.slider("滤波器截止频率 (Hz)", 30, 50, 40, key="fc")
 
         # ====== 信号生成 ======
         t = np.linspace(0, 1, 1000)
